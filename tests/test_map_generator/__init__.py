360ca5c291c82ca01ebccb9fcabde122c9f54283
Filippo Ranza <filipporanza@gmail.com>
2019-08-17 12:10:30 +0200

    improved test files layout

--- tests/test_map_generator/__init__.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/python3
-
-# Copyright (c) 2019 Filippo Ranza <filipporanza@gmail.com>
-
-from .test_container_wrapper import *
-from .test_map_generator import *
--- tests/test_request_generator/__init__.py
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/python3
-
-# Copyright (c) 2019 Filippo Ranza <filipporanza@gmail.com>
