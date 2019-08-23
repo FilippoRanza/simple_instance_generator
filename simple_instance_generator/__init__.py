b6c64eb3933163d9b9d46e569713a8e1283132a3
Filippo <filipporanza@gmail.com>
2019-08-23 11:34:00 +0200

    remove old output generator

--- simple_instance_generator/__init__.py
+++ simple_instance_generator/__init__.py
@@ -5,5 +5,4 @@
 from .map_generator import map_generator
 from .service_generator import service_generator
 from .request_generator import request_generator
-from .output_instance import OutputInstance
 from .serialize import *
